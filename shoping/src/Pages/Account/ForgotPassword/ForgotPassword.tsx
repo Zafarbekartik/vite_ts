fb5a1de5ef924bc1d1907835bff8d218839f9582
zafar <bironmanbek@gmail.com>
2023-09-15 20:11:07 +0500

    HandleSubmitni tikladim

--- shoping/src/Pages/Account/ForgotPassword/ForgotPassword.tsx
+++ shoping/src/Pages/Account/ForgotPassword/ForgotPassword.tsx
@@ -3,10 +3,18 @@ import "./ForgotPassword.css";
 import { NavLink } from "react-router-dom";
 
 function ForgotPassword() {
+  const handleSubmit = (e: FormEvent) => {
+    e.stopPropagation();
+  };
+
   return (
     <div className="logSig">
       <p>! Hisobni tiklash uchun emailingizga ishonch kodlari yuboriladi</p>
-      <form>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <input type="email" placeholder="email" required />
         <button className="logSigBtn">Send</button>
         <p className="backToLogSig">
